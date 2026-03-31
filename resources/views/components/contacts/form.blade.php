bf274e5e79e0d59d6988a1a38ecdc9b7535976fc
Tijs De Mul <tijs.de.mul@gmail.com>
2026-03-31 18:42:10 +0200

    Added empty option

--- resources/views/components/contacts/form.blade.php
+++ resources/views/components/contacts/form.blade.php
@@ -17,7 +17,7 @@
             <div class="sm:col-span-3">
                 <label for="firstName" class="block text-sm/6 font-medium text-gray-900 dark:text-white">First name</label>
                 <div class="mt-2">
-                    <input id="firstName" type="text" name="firstName" autocomplete="given-name" value="{{ old('first_name', $contact->first_name) }}"
+                    <input id="firstName" type="text" name="firstName" autocomplete="given-name" value="{{ old('first_name', $contact->first_name ?? '') }}"
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
                 </div>
                 <x-form.error name='firstName' />
@@ -27,7 +27,7 @@
                 <label for="lastName" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Last
                     name</label>
                 <div class="mt-2">
-                    <input id="lastName" type="text" name="lastName" autocomplete="family-name" value="{{ old('last_name', $contact->last_name) }}"
+                    <input id="lastName" type="text" name="lastName" autocomplete="family-name" value="{{ old('last_name', $contact->last_name ?? '') }}"
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
                 </div>
                 <x-form.error name='lastName' />
@@ -37,7 +37,7 @@
                 <label for="email" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Email
                     address</label>
                 <div class="mt-2">
-                    <input id="email" type="email" name="email" autocomplete="email" value="{{ old('email', $contact->email) }}"
+                    <input id="email" type="email" name="email" autocomplete="email" value="{{ old('email', $contact->email ?? '') }}"
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-basæe text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
                 </div>
                 <x-form.error name='email' />
