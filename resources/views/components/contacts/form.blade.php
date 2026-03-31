eaf37aeef3228c946dcaa01f37ae62d5ddcf239b
Tijs De Mul <tijs.de.mul@gmail.com>
2026-03-31 16:48:30 +0200

    Changed from previous to /contacts

--- resources/views/components/contacts/form.blade.php
+++ resources/views/components/contacts/form.blade.php
@@ -18,6 +18,7 @@
                     <input id="firstName" type="text" name="firstName" autocomplete="given-name"
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
                 </div>
+                <x-form.error name='firstName' />
             </div>
 
             <div class="sm:col-span-3">
@@ -27,6 +28,7 @@
                     <input id="lastName" type="text" name="lastName" autocomplete="family-name"
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
                 </div>
+                <x-form.error name='lastName' />
             </div>
 
             <div class="sm:col-span-6">
@@ -36,13 +38,14 @@
                     <input id="email" type="email" name="email" autocomplete="email"
                         class="block w-full rounded-md bg-white px-3 py-1.5 text-basæe text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
                 </div>
+                <x-form.error name='email' />
             </div>
         </div>
     </div>
 
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
-        <a href="{{ url()->previous() }}" class="text-sm/6 font-semibold text-gray-900 dark:text-white">
+        <a href="/contacts" class="text-sm/6 font-semibold text-gray-900 dark:text-white">
             Cancel
         </a>
         <button type="submit"
