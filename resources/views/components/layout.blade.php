c07699eee14b10dd5a59833ceffbebf75cd76583
Tijs De Mul <tijs.de.mul@gmail.com>
2026-03-31 10:10:35 +0200

    Added nav layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 </head>
 
 <body>
+    <x-nav />
     {{ $slot }}
 </body>
 
