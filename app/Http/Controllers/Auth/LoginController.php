7c05e1d7c4820be3af373db0aa4238c7dd3fa21e
Tijs De Mul <tijs.de.mul@gmail.com>
2026-03-31 10:49:05 +0200

    Added login & logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,31 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function login(Request $request)
     {
-        //
-    }
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $request->session()->regenerate();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+            return redirect('/contacts');
+        }
     }
 
     /**
@@ -42,26 +31,12 @@ class LoginController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
     public function destroy(User $user)
     {
-        //
+        Auth::logout();
+
+        return redirect('/login');
     }
 }
