16cf21493cd0d1f7f2d56d8835d4ab944b6ea00a
Tijs De Mul <tijs.de.mul@gmail.com>
2026-03-31 10:28:51 +0200

    Added routes for login & register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/register', function () {
-    return view('auth.register');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterController::class, 'show']);
+    
+    Route::get('/login', [LoginController::class, 'show']);
 });
