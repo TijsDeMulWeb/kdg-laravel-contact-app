aad68a37ce95d24b27f94b822e419a6a5b616f95
Tijs De Mul <tijs.de.mul@gmail.com>
2026-04-02 10:30:23 +0200

    Changed namespace

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Register\RegisterShowController;
 use App\Http\Controllers\Auth\Register\RegisterStoreController;
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\Login\LoginController;
 use App\Http\Controllers\Auth\Login\LoginShowController;
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/logout', [LogoutController::class]);
-    Route::get('/contacts', [ContactIndexController::class]);
-    Route::get('/contacts/create', [ContactCreateController::class]);
-    Route::post('/contacts/store', [ContactStoreController::class]);
-    Route::delete('/contacts/{contact}', [ContactDeleteController::class]);
-    Route::put('/contacts/{contact}', [ContactActivateController::class])->withTrashed();
-    Route::get('/contacts/{contact}/edit', [ContactEditController::class]);
-    Route::put('/contacts/{contact}/edit', [ContactUpdateController::class]);
+    Route::get('/logout', LogoutController::class);
+    Route::get('/contacts', ContactIndexController::class);
+    Route::get('/contacts/create', ContactCreateController::class);
+    Route::post('/contacts/store', ContactStoreController::class);
+    Route::delete('/contacts/{contact}', ContactDeleteController::class);
+    Route::put('/contacts/{contact}', ContactActivateController::class)->withTrashed();
+    Route::get('/contacts/{contact}/edit', ContactEditController::class);
+    Route::put('/contacts/{contact}/edit', ContactUpdateController::class);
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('/register', [RegisterShowController::class]);
-    Route::post('/register', [RegisterStoreController::class]);
+    Route::get('/register', RegisterShowController::class);
+    Route::post('/register', RegisterStoreController::class);
     
-    Route::get('/login', [LoginShowController::class])->name('login');
-    Route::post('/login', [LoginController::class]);
+    Route::get('/login', LoginShowController::class)->name('login');
+    Route::post('/login', LoginController::class);
 });
   
