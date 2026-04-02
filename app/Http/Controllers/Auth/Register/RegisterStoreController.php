a2df0007340fc404cc9d6a81f9e100c37df623c3
Tijs De Mul <tijs.de.mul@gmail.com>
2026-04-02 11:07:59 +0200

    Deleted field mapping

--- app/Http/Controllers/Auth/Register/RegisterStoreController.php
+++ app/Http/Controllers/Auth/Register/RegisterStoreController.php
@@ -6,20 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class RegisterStoreController extends Controller
 {
     public function __invoke(RegisterUserRequest $request)
     {
-        $request->validated();
-
-        $user = User::create([
-            'first_name' => $request->firstName,
-            'last_name' => $request->lastName,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]); 
+        $user = User::create($request->validated()); 
 
         Auth::login($user);
 
